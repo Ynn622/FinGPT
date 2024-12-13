f2e2a93ccafa903465efe96295f6081a5f55e54a
Yn <applepie930622@gmail.com>
2024-12-14 01:13:42 +0800

    LineBot-FinGPT v12
    init: 將台股成交量改為張數 並加上5MA
    fix: 優化catch_stock

--- function-source/main.py
+++ function-source/main.py
@@ -12,15 +12,29 @@ import os      # 設定環境變數用
 
 # 取得股價資料
 def catch_Stock(stock):
-    data = get_data(stock).round(2).iloc[-25:,:6]
+    try:
+        id = f'{stock}.TW'
+        data = get_data(id)
+        data["volume"]=data["volume"]*0.001
+    except:
+        try:
+            id = f'{stock}.TWO'
+            data = get_data(id)
+            data["volume"]=data["volume"]*0.001
+        except:
+            id = str(stock)
+            data = get_data(id)
+    data = data.round(2).iloc[-25:,:6]
     del data["adjclose"]
-    data.index.name = "Date"
+    # data.index.name = "Date"
     data.index = data.index.strftime("%Y-%m-%d")
+    data["5MA"]=data["close"].rolling(5).mean()
+    data = data.dropna()
     try:
         data = data.drop("2024-11-20")  #資料異常
     except:
         pass
-    return data
+    return data, id
 
 # OpenAI聊天
 def talk(chatbot,record):
@@ -35,13 +49,14 @@ def talk(chatbot,record):
 def generate(input):
     txt = ""
     try:
-        stock_data = catch_Stock(input)
-        txt += f"日期：{stock_data.index[-1]} \n股票代號：{input} \n開盤價：{stock_data.iloc[-1,0]} \n收盤價：{stock_data.iloc[-1,3]}\n\n"
+        stock_data, stock_id = catch_Stock(input)
+        txt += f"日期：{stock_data.index[-1]} \n股票代號：{stock_id} \n開盤價：{stock_data.iloc[-1,0]} \n收盤價：{stock_data.iloc[-1,3]}\n\n"
         # 初始化ai設定
         key = os.environ["OpenAI_key"]
         ai = OpenAI(api_key=key)
         # 對話開始
-        talked = [{"role":"user","content":f"這是台股{input}的資料 請幫我分析\n{stock_data.to_string()} 並給出操作建議"}]
+        talked = [{"role":"user","content":f"這是台股 {stock_id}的資料 請幫我分析\n{stock_data.to_string()} 並給出操作建議"}]
+        del stock_data
         response = talk(ai,talked)
         txt += response.choices[0].message.content
         txt = txt.replace("#","~")
@@ -62,11 +77,7 @@ def linebot(request):
         handler.handle(body, signature)
         msg = json_data['events'][0]['message']['text']
         tk = json_data['events'][0]['replyToken']
-        ans = generate(f"{msg}.TW")
-        if ans == "錯誤！請檢查代碼 或稍後再試～":
-            ans = generate(f"{msg}.TWO")
-        if ans == "錯誤！請檢查代碼 或稍後再試～":
-            ans = generate(str(msg))
+        ans = generate(msg)
         line_bot_api.reply_message(tk,TextSendMessage(ans))
         print(msg, tk)
     except:
