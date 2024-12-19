9aabbababccd1b4279415ce1a5f0fd0120d28645
Yn <applepie930622@gmail.com>
2024-12-20 03:01:40 +0800

    LineBot-FinGPT v16
    init: catch_Stock優化-改用迴圈、上市資料異常改用drop(error)
    generate「收盤價」改成「目前價格」
    大盤新聞抓取優化

--- function-source/main.py
+++ function-source/main.py
@@ -19,34 +19,29 @@ def talk(chatbot,record):
     response = chatbot.chat.completions.create(
         model="gpt-4o-mini",
         messages=record,
-        max_tokens = 15000
+        max_tokens = 16000
     )
     return response
 
 # 取得股價資料
 def catch_Stock(stock):
-    try:
-        id = f'{stock}.TW'
-        data = get_data(id)
-        data["volume"]=data["volume"]*0.001
-    except:
+    for suffix in [".TW",".TWO",""]:
         try:
-            id = f'{stock}.TWO'
+            id = f'{stock}{suffix}'
             data = get_data(id)
-            data["volume"]=data["volume"]*0.001
+            if suffix!="":
+                data["volume"]=data["volume"]*0.001
+            break
         except:
-            id = str(stock)
-            data = get_data(id)
+            continue
     data = data.round(2).iloc[-25:,:6]
     del data["adjclose"]
     # data.index.name = "Date"
     data.index = data.index.strftime("%Y-%m-%d")
     data["5MA"]=data["close"].rolling(5).mean()
     data = data.dropna()
-    try:
-        data = data.drop("2024-11-20")  #資料異常
-    except:
-        pass
+    if id[-3:]==".TW":
+        data = data.drop("2024-11-20", errors='ignore')  # 「上市」資料異常
     return data, id
 
 # 取得股票名稱
@@ -56,51 +51,60 @@ def catch_stock_name(stock_id):
     name = bs(web.text,'html.parser').find_all("h1")[1].text
     return name
 
-# 取得新聞資訊
+# 取得「新聞資訊」
 def get_news(stock_id):
-    stock_name = catch_stock_name(stock_id)
-    url = f"https://ess.api.cnyes.com/ess/api/v1/news/keyword?q={stock_name}&limit=10&page=1"
-    json_news = requests.get(url).json()['data']['items']
-    
-    col = ["Date","Title","text"]
     data = []
-    for item in json_news:
-        id = item['newsId']  
-        title = item['title']
-        t = item['publishAt']+28800
-        news_time = time.strftime("%Y/%m/%d", time.gmtime(t))
-        news_url = f"https://news.cnyes.com/news/id/{id}"
-        news = requests.get(news_url).text
-        news_bs = bs(news,'html.parser')
-        news_find = news_bs.find_all("p")[2:-9]
-        news_data = "\n".join(x.text.strip() for x in news_find)
-        data.append([news_time,title,news_data])
-    
+    col = ["Date","Title","Content"]
+    if stock_id=="^TWII":
+        url = f"https://api.cnyes.com/media/api/v1/newslist/category/tw_stock_news?page=1&limit=10&isCategoryHeadline=1"
+        json_news = requests.get(url).json()['items']['data']
+        for i in range(10):
+            content = json_news[i]["content"].replace("&lt;p&gt;","").replace("&lt;/p&gt;","").replace("&nbsp;","").replace("\n\n","\n")
+            summary = json_news[i]["summary"]
+            t = json_news[i]["publishAt"]+28800
+            news_time = time.strftime("%Y/%m/%d", time.gmtime(t))
+            data.append([news_time,summary,content])
+    else:
+        stock_name = catch_stock_name(stock_id)
+        url = f"https://ess.api.cnyes.com/ess/api/v1/news/keyword?q={stock_name}&limit=10&page=1"
+        json_news = requests.get(url).json()['data']['items']
+        for item in json_news:
+            id = item['newsId']  
+            title = item['title']
+            t = item['publishAt']+28800
+            news_time = time.strftime("%Y/%m/%d", time.gmtime(t))
+            news_url = f"https://news.cnyes.com/news/id/{id}"
+            news = requests.get(news_url).text
+            news_bs = bs(news,'html.parser')
+            news_find = news_bs.find_all("p")[2:-9]
+            news_data = "\n".join(x.text.strip() for x in news_find)
+            data.append([news_time,title,news_data])
+        
     df = pd.DataFrame(data,columns=col)
     df_str = df.to_string().replace("   ","")
     return df_str
 
 # 生成
 def generate(input):
-    txt = ""
+    analysis = ""
     try:
         stock_data, stock_id = catch_Stock(input)
         news_data = get_news(stock_id)
-        txt += f"日期：{stock_data.index[-1]} \n股票代號：{stock_id} \n開盤價：{stock_data.iloc[-1,0]} \n收盤價：{stock_data.iloc[-1,3]}\n\n"
+        analysis += f"日期：{stock_data.index[-1]} \n股票代號：{stock_id} \n開盤價：{stock_data.iloc[-1,0]} \n目前價格：{stock_data.iloc[-1,3]}\n\n"
         # 初始化ai設定
         key = os.environ["OpenAI_key"]
         ai = OpenAI(api_key=key)
         # 對話開始
-        talked = [{"role":"user","content":f"這是台股{stock_id}的資料\n{stock_data.to_string()} 以下是相關新聞可參考：{news_data} 請分析 並給出操作建議"}]
+        talked = [{"role":"user","content":f"這是台股{stock_id}的資料\n{stock_data.to_string()} 以下是相關近期新聞可參考：{news_data} 請分析 並給出操作建議 Reply in 繁體中文"}]
         del stock_data
         response = talk(ai,talked)
-        txt += response.choices[0].message.content
-        txt = txt.replace("#","~")
         print("Token Usage:",response.usage.total_tokens)
+        analysis += response.choices[0].message.content
+        analysis = analysis.replace("#","~")
     except Exception as e:
-        txt = "錯誤！請檢查代碼 或稍後再試～"
+        analysis = "錯誤！請檢查代碼 或稍後再試～"
         print(e)
-    return txt
+    return analysis
 
 # LineBot 接收&傳送
 def linebot(request):
