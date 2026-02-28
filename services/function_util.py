ad93effdad2d9140f4d628e52b77f62467e9844a
Yn <applepie930622@gmail.com>
2026-03-01 01:01:07 +0800

    [fix] yahoo資料源 加入 header、StockList-query_from_yahoo 換 API

--- services/function_util.py
+++ services/function_util.py
@@ -142,7 +142,8 @@ def fetchETFIngredients(ETF_name: str) -> str:
     toolFetchETFIngredients() 會自動調用此函數。
     """
     url = f"https://tw.stock.yahoo.com/quote/{ETF_name}/holding"
-    response = requests.get(url)
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}
+    response = requests.get(url, headers=header, timeout=10)
     soup = bs(response.text, "html.parser")
     table = soup.find_all("ul", class_="Bxz(bb) Bgc($c-light-gray) Bdrs(8px) P(20px)")[1].find_all("li")[1:]
     data = ""
